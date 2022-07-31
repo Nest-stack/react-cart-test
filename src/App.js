4b94db8be9c2efd37bfecb3088c04c046690137e
Nest-stack <nsitseo@gmail.com>
2022-07-31 16:32:21 +0530

    Added an h1

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ const MENU_DATA = [
     id: '0',
     name: 'Sushi',
     description: 'Fish n rice',
-    price: 8
+    price: 8,
   },
   {
     id: '1',
     name: 'Salmon',
     description: 'Type of Fish',
-    price: 2
+    price: 2,
   },
   {
     id: '2',
     name: 'Soya',
     description: 'Veg Item',
-    price: 4
+    price: 4,
   },
   {
     id: '3',
     name: 'Salty Tuna',
     description: 'Salted Fish',
-    price: 1
+    price: 1,
   },
 ];
 
@@ -43,16 +43,16 @@ const App = () => {
 
   return (
     <>
-      {showModal && <Modal clearModal={modalOffHandler}>
-        <Cart
-          clearModal={modalOffHandler}
-          menuData={MENU_DATA}
-        />
-      </Modal>}
+      {showModal && (
+        <Modal clearModal={modalOffHandler}>
+          <Cart clearModal={modalOffHandler} menuData={MENU_DATA} />
+        </Modal>
+      )}
       <Header cartClick={modalShowHandler} />
+      <h1>New feature</h1>
       <ItemList menuData={MENU_DATA} />
     </>
   );
-}
+};
 
 export default App;
